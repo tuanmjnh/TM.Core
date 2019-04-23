86b22237df2e0f8bca7e491807b8a564f4592ba4
tuanmjnh <minhtuan200990@gmail.com>
2019-04-23 23:42:23 +0700

    tm

--- Common/Object.cs
+++ Common/Object.cs
@@ -1,9 +1,12 @@
-﻿namespace TM.Core.Common {
-    public class Extensions {
-        public static readonly string[] Images = new [] { ".jpg", ".jpeg", ".png", ".gif", ".tiff", ".bmp" };
-        public static readonly string[] Excel = new [] { ".xls", ".xlsx", ".csv" };
+﻿namespace TM.Core.Common
+{
+    public class Extensions
+    {
+        public static readonly string[] Images = new[] { ".jpg", ".jpeg", ".png", ".gif", ".tiff", ".bmp" };
+        public static readonly string[] Excel = new[] { ".xls", ".xlsx", ".csv" };
     }
-    public class Directories {
+    public class Directories
+    {
         //Root
         public const string RootDir = "wwwroot\\";
         //Uploads
@@ -20,18 +23,21 @@
         //Language Directories
         public const string languageDir = RootDir + "Language\\";
     }
-    public class FileManager {
+    public class FileManager
+    {
         public const string directory = "Directory";
         public const string file = "File";
     }
-    public class Success {
+    public class Success
+    {
         public const string Get = "Global.msgGetSucsess";
         public const string Create = "Global.msgCreateSucsess";
         public const string Update = "Global.msgUpdateSucsess";
         public const string Delete = "Global.msgDeleteSucsess";
         public const string Recover = "Global.msgRecoverSucsess";
     }
-    public class Error {
+    public class Error
+    {
         public const string DB = "Global.msgDBError";
         public const string Model = "Global.msgModelError";
         public const string IDExist = "Global.msgIDExistError";
@@ -43,13 +49,43 @@
         public const string Recover = "Global.msgRecoverError";
         public const string Exist = "Global.msgErrorExist";
     }
-    public class Permission {
+    public enum Message
+    {
+        success = 1,
+        danger = 2,
+        exist = 3,
+        not_exist = 4,
+        locked = 5,
+        wrong = 6,
+        error_token = 7
+    }
+    public class Permission
+    {
         public const string Select = "GET";
         public const string Create = "POST";
         public const string Edit = "PUT";
         public const string Delete = "DELETE";
     }
-    public class Settings {
-        public string[] modules = new [] { "modules", "default" };
+    public class Settings
+    {
+        public string[] modules = new[] { "modules", "default" };
     }
+    public partial class Paging
+    {
+        private string _sortBy;
+        public string search { get; set; }
+        // public dynamic filter { get; set; }
+        public int totalItems { get; set; }
+        public int page { get; set; }
+        public int rowsPerPage { get; set; }
+        public int flag { get; set; }
+        public bool descending { get; set; }
+        public string sortBy
+        {
+            get => _sortBy;
+            set => _sortBy = value + (descending ? " desc" : "");
+        }
+        public bool isExport { get; set; }
+    }
+
 }
