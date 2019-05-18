9dc67adb2ae67c057b07cda2d7bf826bfc89d317
tuanmjnh <minhtuan200990@gmail.com>
2019-05-18 16:36:11 +0700

    tm

--- Connection/Oracle.cs
+++ Connection/Oracle.cs
@@ -2,20 +2,58 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Oracle.ManagedDataAccess.Client;
-namespace TM.Core.Connection {
-    public class Oracle {
+namespace TM.Core.Connection
+{
+    public class Oracle : System.IDisposable
+    {
         public OracleConnection Connection;
-        public Oracle(string ConnectionString = "VNPTBK") {
-            try {
+        public Oracle(string ConnectionString = "TTKD_BKN")
+        {
+            try
+            {
                 Connection = new OracleConnection(TM.Core.HttpContext.configuration.GetSection($"ConnectionStrings:{ConnectionString}").Value);
-                Connection.Open();
-            } catch (System.Exception) { throw; }
+                if (Connection != null && Connection.State == System.Data.ConnectionState.Closed)
+                    Connection.Open();
+                // if (Connection == null)
+                // {
+                //     Connection.Open();
+                // }
+                // else
+                // {
+                //     if (Connection.State == System.Data.ConnectionState.Closed)
+                //         Connection.Open();
+                // }
+            }
+            catch (System.Exception) { throw; }
         }
-        public void Close() {
-            try {
+        public void Open()
+        {
+            try
+            {
+                if (Connection != null && Connection.State == System.Data.ConnectionState.Closed)
+                    Connection.Open();
+            }
+            catch (Exception) { throw; }
+        }
+        public void Close()
+        {
+            try
+            {
                 if (Connection != null && Connection.State == System.Data.ConnectionState.Open)
+                {
                     Connection.Close();
-            } catch (Exception) { throw; }
+                    Connection.Dispose();
+                }
+            }
+            catch (Exception) { throw; }
+        }
+        void IDisposable.Dispose()
+        {
+            if (Connection != null && Connection.State == System.Data.ConnectionState.Open)
+            {
+                Connection.Close();
+                Connection.Dispose();
+            }
         }
     }
 }
